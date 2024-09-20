13c1ccccfe0444bd1e513c22981f7ccb13e60445
Arhangelov <arhangelov93@gmail.com>
2024-09-20 21:12:38 +0300

    removed unused things

--- client/src/components/TypeOfSushi/TypeOfSushi.jsx
+++ client/src/components/TypeOfSushi/TypeOfSushi.jsx
@@ -1,14 +1,9 @@
-import { useState, useEffect, useCallback, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// import { useCart } from '../../store/CartContext';
-// import { Context } from '../../store/UserContext';
-// import { useCartQty } from '../../store/CartQtyContext';
-
 import { getSushiType } from '../../services/menuService';
-// import { updateCartService } from '../../services/cartService';
 
 import './TypeOfSushi.css';
 import Menu from '../Menu/Menu';
@@ -24,52 +19,6 @@ const TypeOfSushi = () => {
       .then((res) => setSushi(res))
       .catch((error) => console.log(error.message));
   }, [type]);
-
-  // const addToCartHandler = useCallback(
-  //   (id, title, img, price) => {
-  //     const sushiProduct = { id, title, img, price, qty: 1 };
-  //     const currentCartItem = cart.find((item) => item.id === id);
-
-  //     if (currentCartItem) {
-  //       const index = cart.indexOf(currentCartItem);
-  //       cart[index].qty += 1;
-
-  //       setCart([...cart]);
-
-  //       // Notification on successfully added product to cart
-  //       toast.success('🍣 Product is added to the cart!', {
-  //         position: "top-center",
-  //         autoClose: 2000,
-  //         hideProgressBar: true,
-  //         closeOnClick: true,
-  //         pauseOnHover: true,
-  //         draggable: true,
-  //         progress: undefined,
-  //         theme: "dark",
-  //         });
-  //       return updateCartService(cart[index], user.email)
-  //               .then(cart => setCartQty(cart.sumQty));
-  //     }
-
-  //     setCart([ ...cart, sushiProduct ]);
-
-  //     // Notification on successfully added product to cart
-  //     toast.success('🍣 Product is added to the cart!', {
-  //       position: "top-center",
-  //       autoClose: 2000,
-  //       hideProgressBar: true,
-  //       closeOnClick: true,
-  //       pauseOnHover: true,
-  //       draggable: true,
-  //       progress: undefined,
-  //       theme: "dark",
-  //       });
-  //     return updateCartService(sushiProduct, user.email)
-  //             .then(cart => setCartQty(cart.sumQty));
-
-  //   },
-  //   [cart, setCart, setCartQty, user.email]
-  // );
 
   return (
     <div>
